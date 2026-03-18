6ed39b260a3d5941ecae91ee9ef2cf533a3d9d68
Earlysembiring <nathaliasembiring06@gmail.com>
2026-03-18 14:41:11 +0700

    Improve early bootstrap error diagnostics

--- public/index.php
+++ public/index.php
@@ -20,34 +20,44 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
 try {
+    /*
+    |--------------------------------------------------------------------------
+    | Register The Auto Loader
+    |--------------------------------------------------------------------------
+    |
+    | Composer provides a convenient, automatically generated class loader for
+    | this application. We just need to utilize it! We'll simply require it
+    | into the script here so we don't need to manually load our classes.
+    |
+    */
+
+    $autoload = __DIR__.'/../vendor/autoload.php';
+    if (!file_exists($autoload)) {
+        throw new RuntimeException('vendor/autoload.php not found');
+    }
+
+    require $autoload;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Run The Application
+    |--------------------------------------------------------------------------
+    |
+    | Once we have the application, we can handle the incoming request using
+    | the application's HTTP kernel. Then, we will send the response back
+    | to this client's browser, allowing them to enjoy our application.
+    |
+    */
+
+    $bootstrap = __DIR__.'/../bootstrap/app.php';
+    if (!file_exists($bootstrap)) {
+        throw new RuntimeException('bootstrap/app.php not found');
+    }
+
+    $app = require_once $bootstrap;
+
+    $kernel = $app->make(Kernel::class);
     $response = $kernel->handle(
         $request = Request::capture()
     )->send();
