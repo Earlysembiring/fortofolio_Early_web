c3facbabf3c7e33ddba5779bb989aa10e62d2b61
Earlysembiring <nathaliasembiring06@gmail.com>
2026-03-18 14:47:36 +0700

    Fix Vercel runtime cache paths and remove debug hooks

--- public/index.php
+++ public/index.php
@@ -20,51 +20,50 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-try {
-    /*
-    |--------------------------------------------------------------------------
-    | Register The Auto Loader
-    |--------------------------------------------------------------------------
-    |
-    | Composer provides a convenient, automatically generated class loader for
-    | this application. We just need to utilize it! We'll simply require it
-    | into the script here so we don't need to manually load our classes.
-    |
-    */
+if (getenv('VERCEL')) {
+    $cacheDir = '/tmp/bootstrap/cache';
 
-    $autoload = __DIR__.'/../vendor/autoload.php';
-    if (!file_exists($autoload)) {
-        throw new RuntimeException('vendor/autoload.php not found');
+    if (!is_dir($cacheDir)) {
+        @mkdir($cacheDir, 0777, true);
     }
 
-    require $autoload;
-
-    /*
-    |--------------------------------------------------------------------------
-    | Run The Application
-    |--------------------------------------------------------------------------
-    |
-    | Once we have the application, we can handle the incoming request using
-    | the application's HTTP kernel. Then, we will send the response back
-    | to this client's browser, allowing them to enjoy our application.
-    |
-    */
-
-    $bootstrap = __DIR__.'/../bootstrap/app.php';
-    if (!file_exists($bootstrap)) {
-        throw new RuntimeException('bootstrap/app.php not found');
-    }
-
-    $app = require_once $bootstrap;
-
-    $kernel = $app->make(Kernel::class);
-    $response = $kernel->handle(
-        $request = Request::capture()
-    )->send();
-
-    $kernel->terminate($request, $response);
-} catch (\Throwable $e) {
-    http_response_code(500);
-    header('Content-Type: text/plain; charset=UTF-8');
-    echo 'BOOT_ERROR: '.$e->getMessage();
+    putenv('APP_SERVICES_CACHE='.$cacheDir.'/services.php');
+    putenv('APP_PACKAGES_CACHE='.$cacheDir.'/packages.php');
+    putenv('APP_CONFIG_CACHE='.$cacheDir.'/config.php');
+    putenv('APP_ROUTES_CACHE='.$cacheDir.'/routes-v7.php');
+    putenv('APP_EVENTS_CACHE='.$cacheDir.'/events.php');
 }
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
